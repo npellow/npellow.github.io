56b8bee368efe6779cec9fc8c7fb0161c1fbf6f8
Nichole Pellow <mrspellow@comcast.net>
2018-03-12 00:10:08 -0500

    final update

---
+++ project/cart.js
@@ -0,0 +1,147 @@
+//accent page products
+function cartElephant() {
+    var elephantTotal = 29.99;
+    localStorage.setItem("elephant", elephantTotal);
+    if (!Number(localStorage.getItem("elephant")) == 0) {
+      alert('Item has been added to cart');
+  
+    }
+  }
+
+function cartBuddhas(){
+    var buddhaTotal = 14.99;
+    localStorage.setItem("buddhas", buddhaTotal);
+    if (!Number(localStorage.getItem("buddhas")) == 0) {
+        alert('Item has been added to cart');
+    }
+}
+
+function cartChakraTree() {
+    var chakraTotal = 23.99;
+    localStorage.setItem("chakra", chakraTotal);
+    if (!Number(localStorage.getItem("chakra")) == 0) {
+        alert('Item has been added to cart');
+    }
+}
+
+function cartMorLantern() {
+    var lanternTotal = 19.99;
+    localStorage.setItem("lantern", lanternTotal);
+    if (!Number(localStorage.getItem("lantern")) == 0) {
+        alert('Item has been added to cart');
+    }
+}
+
+function cartSelLamp() {
+    var selLampTotal = 39.99;
+    localStorage.setItem("selLamp", selLampTotal);
+    if (!Number(localStorage.getItem("selLamp")) == 0) {
+        alert('Item has been added to cart');
+    }
+}
+
+function cartWoodTable() {
+    var woodTableTotal = 54.99;
+    localStorage.setItem("woodTable", woodTableTotal);
+    if (!Number(localStorage.getItem("woodTable")) == 0) {
+        alert('Item has been added to cart');
+    }
+}
+
+//rugs&ottomans page products
+function cartVintageFancy() {
+    var vintageFancyTotal = 89.99;
+    localStorage.setItem("vintageFancy", vintageFancyTotal);
+    if (!Number(localStorage.getItem("vintageFancy")) == 0) {
+        alert('Item has been added to cart');
+    }
+}
+
+function cartVintageCorbett() {
+    var corbettTotal = 45.99;
+    localStorage.setItem("corbettRug", corbettTotal);
+    if (!Number(localStorage.getItem("corbettRug")) == 0) {
+        alert('Item has been added to cart');
+    }
+}
+
+function cartPersianRug() {
+    var persianTotal = 84.99;
+    localStorage.setItem("persianRug", persianTotal);
+    if (!Number(localStorage.getItem("persianRug")) == 0) {
+        alert('Item has been added to cart');
+    }
+}
+
+function cartPoufCover() {
+    var poufCoverTotal = 14.99;
+    localStorage.setItem("poufCover", poufCoverTotal);
+    if (!Number(localStorage.getItem("poufCover")) == 0) {
+        alert('Item has been added to cart');
+    }
+}
+
+function cartPoufOttoman() {
+    var poufOttomanTotal = 79.99;
+    localStorage.setItem("poufOttoman", poufOttomanTotal);
+    if (!Number(localStorage.getItem("poufOttoman")) == 0) {
+        alert('Item has been added to cart');
+    }
+}
+
+function cartStorageOttoman() {
+    var storageOttomanTotal = 29.99;
+    localStorage.setItem("storageOttoman", storageOttomanTotal);
+    if (!Number(localStorage.getItem("storageOttoman")) == 0) {
+        alert('Item has been added to cart');
+    }
+}
+
+//wall decor products
+function cartMulticolorMandala() {
+    var multiMandalaTotal = 14.99;
+    localStorage.setItem("multiMandala", multiMandalaTotal);
+    if (!Number(localStorage.getItem("multiMandala")) == 0) {
+        alert('Item has been added to cart');
+    }
+}
+
+function cartMacrameWall() {
+    var macrameWallTotal = 18.99;
+    localStorage.setItem("macrameWall", macrameWallTotal);
+    if (!Number(localStorage.getItem("macrameWall")) == 0) {
+        alert('Item has been added to cart');
+    }
+}
+
+function cartMacrameMirror() {
+    var macrameMirrorTotal = 19.99;
+    localStorage.setItem("macrameMirror", macrameMirrorTotal);
+    if (!Number(localStorage.getItem("macrameMirror")) == 0) {
+        alert('Item has been added to cart');
+    }
+}
+
+function cartWallCandle() {
+    var wallCandleTotal = 29.99;
+    localStorage.setItem("wallCandle", wallCandleTotal);
+    if (!Number(localStorage.getItem("wallCandle")) == 0) {
+        alert('Item has been added to cart');
+    }
+}
+
+function cartFeatherWall() {
+    var featherWallTotal = 14.99;
+    localStorage.setItem("featherWall", featherWallTotal);
+    if (!Number(localStorage.getItem("featherWall")) == 0) {
+        alert('Item has been added to cart');
+    }
+}
+
+function cartLightMandala() {
+    var lightMandalaTotal = 15.99;
+    localStorage.setItem("lightMandalaWall", lightMandalaTotal);
+    if (!Number(localStorage.getItem("lightMandalaWall")) == 0) {
+        alert('Item has been added to cart');
+    }
+}
